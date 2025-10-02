ff64175d029335a8a56fd1c617a02d1ceb7badc5
nguyen_dat <manhdatvbhp@gmail.com>
2025-10-02 12:06:48 +0700

    INIT BASE

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  //inject instance of auth service
+  constructor(private authSerice: AuthService) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto): Promise<CreateUserDto> {
+    return await this.authSerice.register(createUserDto);
+  }
+}
